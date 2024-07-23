629f48cf44fa1adca26ab80c83a188f391d52efb
Shouvick21 <bachharshouvickp2@gmail.com>
2024-07-24 01:08:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 load_dotenv()
+import streamlit as st
 
-# defaults to getting the key using os.environ.get("OPENAI_API_KEY")
-# if you saved the key under a different environment variable name, you can do something like:
-client = OpenAI(
-  api_key=os.environ.get("OPENAI_API_KEY"),
-)
 
-completion = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {"role": "system", "content": "You are a proficient email writer,who is skilled at writing profetional email based on user input"},
-    {"role": "user", "content": "write an email for regisnation"}
-  ]
-)
+st.title("shyari generator")
+user_input=st.text_input("enter keyword")
+button=st.button("submit")
+if button and user_input:
+    
+    client = OpenAI(
+      api_key=os.environ.get("api_key"),
+    )
+    completion = client.chat.completions.create(
+      model="gpt-4o-mini",
+      messages=[
+        {"role": "system", "content": "You are good at hindi Shayari "},
+        {"role": "user", "content": f"compose a shyari inhindi language first and then in english based on keyword: {user_input}"}
+      ]
+    )
 
-print(completion.choices[0].message)
-# import openai
-# print(openai.__version__)
+    print(completion.choices[0].message.content)
+    st.text_area("shyeri",completion.choices[0].message.content,height=800)
